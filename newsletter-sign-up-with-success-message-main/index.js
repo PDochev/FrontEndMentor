5520e9a3c59f3d48cb40fc424c112978f61506b1
PDochev <plamendochev0@gmail.com>
2023-06-01 11:39:50 +0300

    email validation

--- newsletter-sign-up-with-success-message-main/index.js
+++ newsletter-sign-up-with-success-message-main/index.js
@@ -4,14 +4,36 @@ const form = document.querySelector("form");
 
 const submitButton = document.getElementById("submit");
 const dismiss = document.getElementById("dismiss");
+const email = document.getElementById("email");
+const invalid = document.getElementById("invalid");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  mainContainer.classList.add("hide");
-  thanksContainer.classList.remove("hide");
+  if (ValidateEmail(email)) {
+    mainContainer.classList.add("hide");
+    thanksContainer.classList.remove("hide");
+  }
 });
 
 dismiss.addEventListener("click", () => {
   mainContainer.classList.remove("hide");
   thanksContainer.classList.add("hide");
+  email.style = "";
+  invalid.style.visibility = "hidden";
 });
+
+function ValidateEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    // document.form.email.focus();
+    return true;
+  } else {
+    // alert("You have entered an invalid email address!");
+    email.style.backgroundColor = "#ffe8e6";
+    email.style.color = "#db9089";
+    email.style.border = "1px solid #db9089";
+    invalid.style.visibility = "visible";
+
+    return false;
+  }
+}
